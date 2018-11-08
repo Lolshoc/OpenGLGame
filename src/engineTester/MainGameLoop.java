2e367619d63904c916c5f4dc496fce1ce08adb6f
lolshoc <lolshoc@gmail.com>
2018-11-07 18:53:22 -0800

    Added terrain.

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -6,12 +6,10 @@ import entities.Light;
 import models.TexturedModel;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
-import renderEngine.DisplayManager;
-import renderEngine.Loader;
+import renderEngine.*;
 import models.RawModel;
-import renderEngine.OBJLoader;
-import renderEngine.Renderer;
 import shaders.StaticShader;
+import terrains.Terrain;
 import textures.ModelTexture;
 
 import java.io.FileNotFoundException;
@@ -22,31 +20,37 @@ public class MainGameLoop {
 
         DisplayManager.createDisplay();
         Loader loader=new Loader();
-        StaticShader shader=new StaticShader();
-        Renderer renderer=new Renderer(shader);
 
 
-        RawModel model= OBJLoader.loadObjModel("dragon", loader);
-        ModelTexture texture=new ModelTexture(loader.loadTexture("gold"));
-        TexturedModel texturedModel=new TexturedModel(model,texture);
-        Entity entity=new Entity(texturedModel,new Vector3f(0,0,-50),0,0,0,1);
-        Light light=new Light(new Vector3f(0,0,-20),new Vector3f(1,1,1));
+
+        RawModel model= OBJLoader.loadObjModel("tree", loader);
+        TexturedModel texturedModel=new TexturedModel(model,new ModelTexture(loader.loadTexture("tree")));
+        ModelTexture texture=texturedModel.getTexture();
+        texture.setShineDamper(10);
+        texture.setRelflectivity(1);
+        Entity entity=new Entity(texturedModel,new Vector3f(0,0,-25),0,0,0,1);
+        Light light=new Light(new Vector3f(2000,2000,2000),new Vector3f(1,1,1));
+        Terrain terrain=new Terrain(0,-1,loader,new ModelTexture(loader.loadTexture("grass")));
+        Terrain terrain2=new Terrain(-1,-1,loader,new ModelTexture(loader.loadTexture("grass")));
         Camera camera=new Camera();
 
+        MasterRenderer renderer=new MasterRenderer();
         while(!Display.isCloseRequested()){
-            entity.increaseRotation(0,1,0);
+            if (entity.getPosition().y > 0.5) {
+                entity.setPosition(new Vector3f(entity.getPosition().x,0.5f,entity.getPosition().z));
+            } else if(entity.getPosition().y==0.5) {
+                entity.setPosition(new Vector3f(entity.getPosition().x, 0f, entity.getPosition().z));
+            }else{
+                entity.increasePosition(0,(float)Math.random(),0);
+            }
             camera.move();
-            renderer.prepare();
-            shader.start();
-            shader.loadLight(light);
-            shader.loadViewMatrix(camera);
-            renderer.render(entity,shader);
-            shader.stop();
+            renderer.processTerrain(terrain);
+            renderer.processTerrain(terrain2);
+            renderer.processEntity(entity);
+            renderer.render(light,camera);
             DisplayManager.updateDisplay();
-
         }
-
-        shader.cleanUp();
+        renderer.cleanUp();
         loader.cleanUp();
         DisplayManager.closeDisplay();
 
--- src/textures/ModelTexture.java
+++ src/textures/ModelTexture.java
@@ -4,12 +4,30 @@ public class ModelTexture {
 
     private int textureID;
 
+    private float shineDamper=1;
+    private float reflectivity=1;
+
     public ModelTexture(int id){
         this.textureID=id;
     }
 
-    public int getTextureID(){
-        return this.textureID;
+    public int getTextureID() {
+        return textureID;
     }
 
+    public float getShineDamper() {
+        return shineDamper;
+    }
+
+    public void setShineDamper(float shineDamper) {
+        this.shineDamper = shineDamper;
+    }
+
+    public float getRelflectivity() {
+        return reflectivity;
+    }
+
+    public void setRelflectivity(float relflectivity) {
+        this.reflectivity = relflectivity;
+    }
 }
